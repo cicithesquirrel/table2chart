3a3aa554c2a4d6ca75e3b2a5a4ab82fbbcbac987
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2015-10-12 20:32:23 +0200

    Added: Use RequireJS to load JQuery in tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,13 @@
 require.config({
-    baseUrl: '../src'
+    baseUrl: '../src',
+    paths: {
+        "jquery": "../bower_components/jquery/dist/jquery"
+    }
 });
 
-require(['table2chart'],
-    function (t2c) {
+require(['table2chart', 'jquery'],
+    function (t2c, $) {
+        // on-load, apply charting
         $(function () {
             $('[data-t2c]').each(function (indexInArray, o) {
                 t2c.apply(o);

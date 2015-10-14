86dce4bda72324e2b2bbbb01e023ea4f49114067
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2015-10-14 20:45:59 +0200

    Added Gulp generation

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/* jshint node: true */
+"use strict";
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var concat = require('gulp-concat');
+
+var paths = {
+    scripts: ['src/*.js'],
+    build: 'table2chart.min.js'
+};
+
+gulp.task('clean', function (endCallback) {
+    del(['build']);
+    endCallback();
+});
+
+gulp.task('scripts', ['clean'], function () {
+    // Minify and copy all JavaScript (except vendor scripts)
+    // with sourcemaps all the way down
+    return gulp.src(paths.scripts)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat(paths.build))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['clean', 'scripts']);
